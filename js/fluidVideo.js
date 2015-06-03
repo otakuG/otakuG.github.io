8597d229c72b2a7f6571848e7f3cd68c4c2c8fe6
OTAKU_G <gt689229@gmail.com>
2015-06-04 04:28:23 +0800

    Site updated: 2015-06-04 04:28:23

--- js/fluidVideo.js
+++ js/fluidVideo.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+// 取得頁面中嵌入的的 YouTube 影片
+var allVideos = $("iframe[src*='//www.youtube.com']")
+
+// 把所有的 iframe 包一個 videoWrapper 容器（父元素）
+$(allVideos).each(function() {
+	$(this).wrap("<div class='videoWrapper'></div>")
+});
